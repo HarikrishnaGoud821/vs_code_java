84972de7b686ac1531832d86ed8cf73690ca2def
harikrishnagoud821 <goudharikrishna82@gmail.com>
2026-02-15 00:43:46 +0530

    your message

---
+++ Data_type/Primitive/Char.java
@@ -0,0 +1,7 @@
+public class Char{
+    public static void main(String[] args){
+        char letter1 = 'A';
+        //char letter2 = 'AB'; // This will cause a compilation error because a char can only hold a single character
+        System.out.println("The value of letter1 is: " + letter1);
+    }
+}
