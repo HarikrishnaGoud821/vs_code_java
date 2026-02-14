0d428329e9da56cb57ae8a7d1667768e4fa52860
harikrishnagoud821 <goudharikrishna82@gmail.com>
2026-02-14 11:46:45 +0530

    first commit

---
+++ Data_type/Bool.java
@@ -0,0 +1,7 @@
+public class Bool{
+    public static void main(String[] args) {
+        boolean isjavafun = true;
+        boolean isjavaisodd = false;
+        System.out.println("Is java fun:" + isjavafun);
+        System.out.println("Is java is odd:" + isjavaisodd);
+    }}
---
+++ Data_type/Byte.java
@@ -0,0 +1,6 @@
+public class Byte{
+    public static void main(String[] args) {
+        byte num1 = 20;
+        //byte num2 = 300; // This will cause a compilation error because 300 is out of the byte range (-128 to 127)
+        System.out.println("The value of num1 is: " + num1);
+    }}
